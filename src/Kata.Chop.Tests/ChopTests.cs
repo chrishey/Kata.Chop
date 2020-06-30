6a448b0565e78df99eff48b4b0982f217d0aa37f
Chris Hey <chris.hey@slicedbread.co.uk>
2020-06-30 16:48:18 +0100

    First passing test for finding int in array

--- src/Kata.Chop.Tests/ChopTests.cs
+++ src/Kata.Chop.Tests/ChopTests.cs
@@ -1,6 +1,26 @@
-﻿namespace Kata.Chop.Tests
+﻿using System.Collections.Generic;
+using Should;
+using Xunit;
+
+namespace Kata.Chop.Tests
 {
     public class ChopTests
     {
+        [Theory]
+        [ClassData(typeof(TestData))]
+        public void FindIntInArray(int intToFind, int[] arrayOfInts, int expectedPosition)
+        {
+            var position = new Chop().Find(intToFind, arrayOfInts);
+
+            position.ShouldEqual(expectedPosition);
+        }
+    }
+
+    public class TestData : TheoryData<int, int[], int>
+    {
+        public TestData()
+        {
+            Add(1, new List<int>{1,2,3,4}.ToArray(), 0);
+        }
     }
 }
--- src/Kata.Chop/Chop.cs
+++ src/Kata.Chop/Chop.cs
@@ -2,5 +2,9 @@
 {
     public class Chop
     {
+        public int Find(int intToFind, int[] array)
+        {
+            return 0;
+        }
     }
 }
